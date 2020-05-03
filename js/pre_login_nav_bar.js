aa2adc7314691a7544785767e767dd54eb18dde1
JackShih <pyopoly@gmail.com>
2020-05-02 23:14:33 -0700

    responsive navbar index login features about pages

--- js/pre_login_nav_bar.js
+++ js/pre_login_nav_bar.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
-    console.log("ready");
+    console.log("read1y");
 
     $("#bars").on("click", showNav);
     $("#navPopupBack").on("click", hideNav);
@@ -9,12 +9,14 @@ $(document).ready(function() {
         $("header").css("display", "none");
         $("main").css("display", "none");
         $("#navPopUp").css("display", "block");  
+        $("footer").css("display", "none");  
     }
 
     function hideNav() {
         $("header").css("display", "flex");
         $("main").css("display", "block");
         $("#navPopUp").css("display", "none");  
+        $("footer").css("display", "block");
     }
 })
 
