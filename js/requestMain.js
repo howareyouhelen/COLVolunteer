e947e74306121b9567cb45ef44b66dc63a1035a1
Helen <howareyouhelen@hotmail.com>
2020-05-07 21:08:22 -0700

    adding voluteer's uid (current user's uid) when they click on "help me" button on requestMain page

--- js/requestMain.js
+++ js/requestMain.js
@@ -2,6 +2,12 @@ $(document).ready(function () {
     const list_div = document.querySelector("#list_div");
     var reqRef = db.collection("requestpost");
 
+    firebase.auth().onAuthStateChanged(function(user) {
+        var user = user.uid;
+        currentUser = user;
+    });
+
+    //display all request posts dynamically
     reqRef.onSnapshot(function(querySnapshot){
         querySnapshot.forEach(function(doc){
             list_div.innerHTML += "<div class= 'list-item'><p>items: </p>" 
@@ -10,9 +16,22 @@ $(document).ready(function () {
                 + doc.data().needbydate 
                 + "<p>message: </p>" 
                 + doc.data().message
+                + "<p class='docref' style='visibility: hidden'>"
+                + doc.data().docRefid
+                + "</p>"
                 + "<input id='help_btn' class='btn btn-primary' type='submit' value='Help Me'/>"
-                + "</div>"
+                + "</div>";
+
+            $(".btn").click(function(){
+                $(this).val("Help on its way!").prop('disabled', true);
+                var matchID = $(this).siblings('.docref')[0].innerHTML;
+                var docRef = reqRef.doc(matchID);
+                docRef.set({
+                    volunteer_uid: currentUser
+                }, {merge: true});
+           })
         });
-    });   
+    });
+
     
 });
