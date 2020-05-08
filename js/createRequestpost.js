bd7b81f38ab18770ca93addeaa4eea3e20efcf04
Helen <howareyouhelen@hotmail.com>
2020-05-07 21:07:27 -0700

    adding current user's uid (requester uid) and docRef id into the db when create post

--- js/createRequestpost.js
+++ js/createRequestpost.js
@@ -3,38 +3,50 @@ $(document).ready(function () {
     var reqform = document.getElementById("create-reqpost-form")
     reqform.addEventListener('submit', submitForm);
 
+    firebase.auth().onAuthStateChanged(function(user) {
+        var user = user.uid;
+        currentUser = user;
+    });
+
+
     // take in an event and submit form
     function submitForm(event){
-    event.preventDefault();
-    var date = new Date();
-    var gettimestamp = date.getTime();
-    // get values
-    var items = getInputVal("items");
-    var needbydate = getInputVal("needbydate");
-    var message = getInputVal("message");
-    var timestamp = gettimestamp;
+        event.preventDefault();
+        var date = new Date();
+        var gettimestamp = date.getTime();
+        // get values
+        var items = getInputVal("items");
+        var needbydate = getInputVal("needbydate");
+        var message = getInputVal("message");
+        var timestamp = gettimestamp;
+        var postrequester_uid = currentUser;
 
-    console.log(needbydate);
-    console.log(items);
-
-    //save request post
-    savePost(items, needbydate, message, timestamp);
+        //save request post
+        savePost(items, needbydate, message, timestamp, postrequester_uid);
     }
     
+
+
+  
+
     // Function to get form values 
     function getInputVal(id) {
         return $("#"+id).val();
     }
 
     // save requestpost to firebase
-    function savePost (items, needbydate, message, timestamp) {
+    function savePost (items, needbydate, message, timestamp, postrequester_uid) {
         db.collection("requestpost").add({
             items: items,
             needbydate: needbydate,
             message: message,
-            timestamp: timestamp
+            timestamp: timestamp,
+            postrequester_uid: postrequester_uid
         })
         .then(function(docRef) {
+            docRef.set({
+                docRefid: docRef.id
+            }, {merge: true});
             console.log("Document written with ID: ", docRef.id);
             $("form").trigger("reset");
         })
