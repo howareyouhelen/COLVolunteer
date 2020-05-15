438bad6ba9bee14f76f23c7fb164773eb0631946
JackShih <pyopoly@gmail.com>
2020-05-14 22:27:22 -0700

    css changes

--- js/main.js
+++ js/main.js
@@ -1,34 +1,35 @@
-
 // working with notifications
 function workingWithNotifications() {
     // console.log("in new function");
 
     auth.onAuthStateChanged((user) => {
-        if(user){
+        if (user) {
             db.collection('user').doc(user.uid).onSnapshot(function (snap) {
                 var newrequest = snap.data().newMsg;
                 // console.log("current data is ...", snap.data().newMsg);
-                if(newrequest == true){
+                if (newrequest == true) {
                     alert("you have a new request");
-                    var notify = {"background-color":"red"};
+                    var notify = {
+                        "background-color": "red"
+                    };
                     $("#pendingRequests").css(notify);
                 }
             });
-            }
-        })
+        }
+    })
 }
 
 
 //Sends the request to the volunteer --> shoppingList + msg
 function sendRequest(callback) {
-    $("#send-request").on('click', ()=>{
+    $("#send-request").on('click', () => {
         let requestMsg = $('#msgEntry').val();
         auth.onAuthStateChanged((user) => {
             if (user) {
                 let my = db.collection('user').doc(user.uid);
                 let userCurList = my.collection('shoppingList').doc('currentList');
-                my.get().then((doc)=>{
-                    volPostId  = doc.data().currentVolpostDocId;
+                my.get().then((doc) => {
+                    volPostId = doc.data().currentVolpostDocId;
                     db.collection('volunteerPosts').doc(volPostId).get().then((snap1) => {
                         let volUId = snap1.data().volUId;
                         userCurList.onSnapshot(function (snap2) {
@@ -36,27 +37,39 @@ function sendRequest(callback) {
                             let volunteerMsg = volunteer.collection('requestForMe').doc();
                             console.log(volunteerMsg.id);
                             var docid = volunteerMsg.id;
-                        
+
                             console.log(user.uid);
                             volunteerMsg.set({
-                                
-                                list    : snap2.data().list,
-                                message : requestMsg,
-                                fromUserId  : user.uid,
-                                volPostDocId : volPostId,
-                                myUID : volUId,
+
+                                list: snap2.data().list,
+                                message: requestMsg,
+                                fromUserId: user.uid,
+                                volPostDocId: volPostId,
+                                myUID: volUId,
                                 docRefid: docid,
                                 reqAccepted: false
                             })
-                        volunteer.set({ newMsg: true }, {merge: true});
-                        volunteer.set({ newReq: true }, {merge: true});
+                            volunteer.set({
+                                newMsg: true
+                            }, {
+                                merge: true
+                            });
+                            volunteer.set({
+                                newReq: true
+                            }, {
+                                merge: true
+                            });
                         })
                     })
                     callback(volPostId);
-                    my.collection('pastRequestsToOthers').add({ volPostDocId : volPostId});
+                    my.collection('pastRequestsToOthers').add({
+                        volPostDocId: volPostId
+                    });
                 })
-            } else {console.log("no user");}
-        })  
+            } else {
+                console.log("no user");
+            }
+        })
     })
 }
 
@@ -64,7 +77,7 @@ function sendRequest(callback) {
 function makeARequestButtonDone(uid) {
     let buttons = $('button.make-request');
     for (let x = 0; x < buttons.length; x++) {
-        if(buttons[x].value === uid) {
+        if (buttons[x].value === uid) {
             $(buttons[x]).html("Request Sent");
             $(buttons[x]).prop("disabled", true);
         }
@@ -78,10 +91,14 @@ function makeRequestButtonEvent() {
     console.log(name)
     $('#volunteerName').html("Send your request to " + name);
     auth.onAuthStateChanged((user) => {
-        if(user) {
+        if (user) {
             let myCollection = db.collection('user').doc(user.uid);
             let postId = $(event)[0].currentTarget.value;
-            myCollection.set({ currentVolpostDocId: postId }, {merge: true});
+            myCollection.set({
+                currentVolpostDocId: postId
+            }, {
+                merge: true
+            });
         }
     })
     showCurrentList();
@@ -106,9 +123,9 @@ function showCurrentList() {
 // Deactivates previously made request posts
 function deactivatePastRequests() {
     auth.onAuthStateChanged((user) => {
-        if(user){
-            db.collection('user').doc(user.uid).collection('pastRequestsToOthers').get().then((snap)=>{
-                snap.forEach((doc)=> {
+        if (user) {
+            db.collection('user').doc(user.uid).collection('pastRequestsToOthers').get().then((snap) => {
+                snap.forEach((doc) => {
                     vPoId = doc.data().volPostDocId;
                     makeARequestButtonDone(vPoId);
                 })
@@ -119,6 +136,7 @@ function deactivatePastRequests() {
 
 
 function dropPin() {
+    hideDistance()
     markers.forEach(function (marker) {
         marker.setMap(null);
     });
@@ -126,7 +144,10 @@ function dropPin() {
     let store = $(event.currentTarget.parentNode).children()[1].innerText;
     let storelat = parseFloat($(event.currentTarget).attr('lat'));
     let storelng = parseFloat($(event.currentTarget).attr('lng'));
-    let latlng = { lat: storelat, lng: storelng}
+    let latlng = {
+        lat: storelat,
+        lng: storelng
+    }
 
     var storeMarker = new google.maps.Marker({
         position: latlng,
@@ -145,7 +166,7 @@ function dropPin() {
 
 // Sorts the volPosts by distance and stores the order of volpostdocid in the meta data of the user
 function sortPostsInDB() {
-    firebase.auth().onAuthStateChanged(function(user) {
+    firebase.auth().onAuthStateChanged(function (user) {
         if (user) {
             let postOrder = db.collection('user').doc(user.uid).collection('metaData').doc('volPostOrder').collection('order');
             let myMap = db.collection('user').doc(user.uid).collection('metaData').doc('map');
@@ -155,9 +176,11 @@ function sortPostsInDB() {
                     querySnapshot.forEach(function (doc) {
                         /* get volunteer post data from db */
                         var docId = doc.id;
-                        var storeGeopoint = doc.data().geopoint;  //geopoint._lat
+                        var storeGeopoint = doc.data().geopoint; //geopoint._lat
                         let distance = geopoint_distance(myGeopoint, storeGeopoint);
-                        postOrder.doc("" + distance).set({docId : docId})
+                        postOrder.doc("" + distance).set({
+                            docId: docId
+                        })
                     })
                 }).then(displayPosts)
             })
@@ -165,29 +188,35 @@ function sortPostsInDB() {
     })
 }
 
+function hideDistance() {
+    $('.distance').on('click', function() {
+        $('.distance').hide();
+    })
+}
+
 // displays the volposts according to the order in the user's metadata in the database
 function displayPosts() {
     console.log("displaying posts")
-    firebase.auth().onAuthStateChanged(function(user) {
+    firebase.auth().onAuthStateChanged(function (user) {
         if (user) {
             let order = db.collection('user').doc(user.uid).collection('metaData').doc('volPostOrder').collection('order');
-            order.get().then((data)=>{
+            order.get().then((data) => {
                 var x = 1;
-                data.forEach(function(item) {
+                data.forEach(function (item) {
                     let vdocid = item.data().docId;
-                    db.collection("volunteerPosts").doc(vdocid).get().then((doc)=>{
+                    db.collection("volunteerPosts").doc(vdocid).get().then((doc) => {
                         var store = doc.data().store;
                         var storeAddress = doc.data().storeAddress;
                         var volname = doc.data().volname;
                         // var email = doc.data().email;
                         var date = doc.data().date;
-                        var storeGeopoint = doc.data().geopoint; 
+                        var storeGeopoint = doc.data().geopoint;
                         var lat = doc.data().geopoint._lat + "";
                         var lng = doc.data().geopoint._long;
                         var docId = doc.id;
-                                
+
                         var y = "card" + x;
-                        
+
                         var z = '<div class="card" id="' + y + '"></div>';
                         var z_id = '#' + y;
 
@@ -217,21 +246,21 @@ function displayPosts() {
                         var f_id = '#' + y + 'date';
 
                         $(b_id).append(f);
-                        
+
                         let info = 'class="btn btn-outline-info make-request" data-toggle="modal" data-target="#exampleModal" onclick="makeRequestButtonEvent()"';
                         var e = '<button type="button"' + info + 'id="' + y + 'button" value = ' + docId + '>' + 'Send Request</button>';
                         $(b_id).append(e);
 
                         x++;
-                
+
                         let myMap = db.collection('user').doc(user.uid).collection('metaData').doc('map');
                         myMap.get().then(snap => {
                             let myGeopoint = snap.data().geolocation;
                             let distance = geopoint_distance(myGeopoint, storeGeopoint);
-                            
+
                             var pin = `<i class='fas fa-map-marker-alt' lat="${lat}" lng="${lng}" onclick="dropPin()"></i>`;
                             var btn = `<button type="button" class="btn btn-outline-primary btn-sm show-on-map" lat="${lat}" lng="${lng}" onclick="dropPin()">show on map</button>`
-                            var dis = '<div style="float:right" class="card-title" id="' + y + 'distance">' + distance + '</div>'  + btn+ pin;
+                            var dis = '<div style="float:right" class="card-title distance" id="' + y + 'distance">' + distance + '</div>' + btn + pin;
 
                             $(b_id).append(dis);
                         })
