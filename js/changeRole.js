f2b2d64a6133648b43e266a2c54218260bc6097a
JackShih <pyopoly@gmail.com>
2020-05-15 16:01:45 -0700

    reverse geocode

--- js/changeRole.js
+++ js/changeRole.js
@@ -1,25 +1,17 @@
+
+
+
+
 //Converts address into geolocation and sets in the user's database
 function userGeopoint() {
     auth.onAuthStateChanged(function (user) {
         db.collection('user').doc(user.uid).get().then(snap => {
             let address = snap.data().address;
             let string = address.split(" ").join("+");
-            const Url = 'https://maps.googleapis.com/maps/api/geocode/json?address=' + string + '&key=' + firebaseKEY;
-            $.ajax({
-                url: Url,
-                type: "GET",
-                success: function (response) {
-                    let userGeopoint = db.collection('user').doc(user.uid).collection('metaData').doc('map');
-                    let location = response.results[0].geometry.location;
-                    userGeopoint.set({
-                        geolocation: new firebase.firestore.GeoPoint(location.lat, location.lng)
-                    });
-                }
-            })
+            
         })
     })
 }
-
 
 // This find the current positino of user and saves it to Database
 function getUserLocation() {
@@ -43,6 +35,18 @@ function getUserLocation() {
                 geolocation: new firebase.firestore.GeoPoint(lat, lng)
             });
 
+            const Url = 'https://maps.googleapis.com/maps/api/geocode/json?latlng=' + lat + ',' + lng + '&key=' + firebaseKEY;
+            $.ajax({
+                url: Url,
+                type: "GET",
+                success: function (response) {
+                    userAddress = response.results[1].formatted_address;
+                    let userAD = db.collection('user').doc(user.uid);
+                    userAD.set({
+                        address: userAddress
+                    },{ merge: true });
+                }
+            })
         })
     }
 }
