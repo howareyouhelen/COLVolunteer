2e57f983989f6166fae9cc402d0553af830fd030
Animesh <annimesh7@gmail.com>
2020-05-21 12:08:37 -0700

    commented js files

--- js/request.js
+++ js/request.js
@@ -1,23 +1,31 @@
-$(document).ready(()=>{
+/*
+all of the console.log statements are used for testing
+*/
+
+$(document).ready(() => {
     console.log("in js file");
     loadrequests();
-    
+
 })
 
 
 
-function loadrequests(){
+function loadrequests() {
 
-
-    auth.onAuthStateChanged((user) => {//getting user
+    auth.onAuthStateChanged((user) => { //getting user
         console.log(user);
 
-        if(user){//if user exists
+        if (user) { //if user exists
 
-            db.collection("user").doc(user.uid).set(
-                {newMsg: false},{merge:true}
-            );
-
+            db.collection("user").doc(user.uid).set({
+                newMsg: false
+            }, {
+                merge: true
+            });
+            
+            /*
+            loading all the requests by iterating through requestforMe sub-collection in user's doc
+            */
 
             db.collection("user").doc(user.uid).collection("requestForMe").get().then((querySnapshot) => {
                 var x = 1;
@@ -25,157 +33,185 @@ function loadrequests(){
 
                     var istrue = doc.data().reqAccepted;
                     var isdeclined = doc.data().reqDeclined;
-                    if(istrue == false && isdeclined == false){
-                    var requesterId = doc.data().fromUserId;
-                    db.collection("user").doc(requesterId).get().then((snap1 =>{
-                        var requesterName = snap1.data().name;
-                        //var requesterAddress = snap1.data().address;
-                        var requesterEmail = snap1.data().email;
-                        
+                    if (istrue == false && isdeclined == false) {  //checking if the request is new or has already been viewed.
+                        var requesterId = doc.data().fromUserId;
+                        db.collection("user").doc(requesterId).get().then((snap1 => {
+                            var requesterName = snap1.data().name;
+                            //var requesterAddress = snap1.data().address;
+                            var requesterEmail = snap1.data().email;
 
-                    console.log("test..." + requesterEmail);
-                    console.log("test..." + requesterName);
-                    var message = doc.data().message;
-                    var list = doc.data().list;
-                    var docRef = doc.data().docRefid;
-                    
-                    console.log(message);
-                    console.log(list);
-                    console.log(requesterId);
-                    console.log(requesterName);
-                    //console.log(requesterAddress);
 
-                    var y = "card" + x;
+                            console.log("test..." + requesterEmail);
+                            console.log("test..." + requesterName);
+                            var message = doc.data().message;
+                            var list = doc.data().list;
+                            var docRef = doc.data().docRefid;
 
-                    var z = '<div class="card" id="' + y + '"></div>';
-                    var z_id = '#' + y;
+                            console.log(message);
+                            console.log(list);
+                            console.log(requesterId);
+                            console.log(requesterName);
+                            //console.log(requesterAddress);
+                             /*
+                            displaying requests in cards with bootstrap and jquerry
+                             */
+                            var y = "card" + x;
 
-                    $("#item_cards").append(z);
+                            var z = '<div class="card" id="' + y + '"></div>';
+                            var z_id = '#' + y;
 
-                    var b = '<div class="card-body" id="' + y + 'body">';
-                    var b_id = '#' + y + 'body';
+                            $("#item_cards").append(z);
 
-                    $(z_id).append(b);
+                            var b = '<div class="card-body" id="' + y + 'body">';
+                            var b_id = '#' + y + 'body';
 
-                    var c = '<h4 class="card-title" id="' + y + 'name">Requester: ' + requesterName + '</h4>';
-                    var c_id = '#' + y + 'name';
+                            $(z_id).append(b);
 
-                    $(b_id).append(c);
+                            var c = '<h4 class="card-title" id="' + y + 'name">Requester: ' + requesterName + '</h4>';
+                            var c_id = '#' + y + 'name';
 
-                    // var d = '<h6 class="card-subtitle mb-2 text-muted" id="' + y + 'address">Requester\'s address: ' + requesterAddress + '</h6>';
-                    // var d_id = '#' + y + 'address';
+                            $(b_id).append(c);
 
-                    var da = '<h6 class="card-subtitle mb-2 text-muted" id="' + y + 'email">Requester\'s email: ' + requesterEmail + '</h6>';
-                    var d_id = '#' + y + 'email';
+                            // var d = '<h6 class="card-subtitle mb-2 text-muted" id="' + y + 'address">Requester\'s address: ' + requesterAddress + '</h6>';
+                            // var d_id = '#' + y + 'address';
 
-                    $(b_id).append(da);
+                            var da = '<h6 class="card-subtitle mb-2 text-muted" id="' + y + 'email">Requester\'s email: ' + requesterEmail + '</h6>';
+                            var d_id = '#' + y + 'email';
 
-                    var f = '<p class="card-text" id="' + y + 'message"> Message: ' + message + '</p>';
-                    var f_id = '#' + y + 'message';
+                            $(b_id).append(da);
 
-                    var g = '<p class="card-text" id="' + y + 'list"> Items requested: ' + list + '</p>';
-                    var g_id = '#' + y + 'list';
+                            var f = '<p class="card-text" id="' + y + 'message"> Message: ' + message + '</p>';
+                            var f_id = '#' + y + 'message';
 
-                    $(b_id).append(f);
-                    $(b_id).append(g);
-                    
-                    
+                            var g = '<p class="card-text" id="' + y + 'list"> Items requested: ' + list + '</p>';
+                            var g_id = '#' + y + 'list';
 
-                    var h = '<button type="button" class="btn btn-success" id=' + docRef + ' value = ' + docRef + ' onclick = "requestAccepted(this.id)" >' + 'Accept</button>';
-                    $(b_id).append(h);
+                            $(b_id).append(f);
+                            $(b_id).append(g);
 
-                    var i = '<button  type="button" class="btn btn-danger" id=' + docRef + ' value = ' + docRef + ' onclick = "requestDeclined(this.id)">' + 'Decline</button>';
-                    $(b_id).append(i);
-                    
-                    x++;
-                    
-                }))}
+
+
+                            var h = '<button type="button" class="btn btn-success" id=' + docRef + ' value = ' + docRef + ' onclick = "requestAccepted(this.id)" >' + 'Accept</button>';
+                            $(b_id).append(h);
+
+                            var i = '<button  type="button" class="btn btn-danger" id=' + docRef + ' value = ' + docRef + ' onclick = "requestDeclined(this.id)">' + 'Decline</button>';
+                            $(b_id).append(i);
+
+                            x++; // incrementing x variable for card numbers.
+
+                        }))
+                    }
                 })
-            })         
-        }          
-    });               
+            })
+        }
+    });
 }
 
-function requestAccepted(clicked_id){
+/*
+accept button,
+change the status of request to accepted for volunteer as well as help-seeker.
+*/
+
+function requestAccepted(clicked_id) {
     console.log("request accepted");
     console.log(clicked_id);
     auth.onAuthStateChanged((user) => {
         db.collection("user").doc(user.uid).get().then(snap => {
             var volName = snap.data().name;
-        
-        db.collection("user").doc(user.uid).collection("requestForMe").doc(clicked_id).set(
-            {reqAccepted:true,reqCompleted:false},{merge:true}
-        )
-        db.collection("user").doc(user.uid).collection("requestForMe").doc(clicked_id).get().then(function(doc){
-            var requesterId = doc.data().fromUserId;
-            var volunteerPostId = doc.data().volPostDocId;
-            
-            console.log(requesterId);
-            console.log(volunteerPostId);
-            console.log(volName);
-            db.collection("user").doc(requesterId).collection("pastRequestsToOthers").where("volPostDocId","==",volunteerPostId).get().
+
+            db.collection("user").doc(user.uid).collection("requestForMe").doc(clicked_id).set({
+                reqAccepted: true,
+                reqCompleted: false
+            }, {
+                merge: true
+            })
+            db.collection("user").doc(user.uid).collection("requestForMe").doc(clicked_id).get().then(function (doc) {
+                var requesterId = doc.data().fromUserId;
+                var volunteerPostId = doc.data().volPostDocId;
+
+                console.log(requesterId);
+                console.log(volunteerPostId);
+                console.log(volName);
+                db.collection("user").doc(requesterId).collection("pastRequestsToOthers").where("volPostDocId", "==", volunteerPostId).get().
                 then(querySnapshot => {
                     querySnapshot.forEach(documentSnapshot => {
                         var querry = documentSnapshot.ref.path;
                         console.log(querry);
-                      console.log(`Found document at ${documentSnapshot.ref.path}`);
-                      var d = querry.split('/');
-                      console.log(d);
-                      var c = d[3];
-                      console.log(c);
-                      db.collection("user").doc(requesterId).collection("pastRequestsToOthers").doc(c).set(
-                        {reqAccepted:true,reqCompleted:false,volunteerName:volName},{merge:true}
-                      ).then(location.reload())
+                        console.log(`Found document at ${documentSnapshot.ref.path}`);
+                        var d = querry.split('/');
+                        console.log(d);
+                        var c = d[3];
+                        console.log(c);
+                        db.collection("user").doc(requesterId).collection("pastRequestsToOthers").doc(c).set({
+                            reqAccepted: true,
+                            reqCompleted: false,
+                            volunteerName: volName
+                        }, {
+                            merge: true
+                        }).then(location.reload())
+                    })
+
+
+                })
             })
-            
-            
+
         })
     })
-    
-})
-})
 
 }
 
-function requestDeclined(clicked_id){
-    
+/*
+decline button,
+change the status of request to declined for volunteer as well as help-seeker.
+And deletes the request from volunteer's database.
+*/
+
+function requestDeclined(clicked_id) {
+
     console.log("request declined");
     console.log(clicked_id);
 
     auth.onAuthStateChanged((user) => {
         db.collection("user").doc(user.uid).get().then(snap => {
             var volName = snap.data().name;
-        
-        db.collection("user").doc(user.uid).collection("requestForMe").doc(clicked_id).set(
-            {reqDeclined:true,reqCompleted:false},{merge:true}
-        )
-        db.collection("user").doc(user.uid).collection("requestForMe").doc(clicked_id).get().then(function(doc){
-            var requesterId = doc.data().fromUserId;
-            var volunteerPostId = doc.data().volPostDocId;
-            
-            console.log(requesterId);
-            console.log(volunteerPostId);
-            console.log(volName);
-            db.collection("user").doc(requesterId).collection("pastRequestsToOthers").where("volPostDocId","==",volunteerPostId).get().
+
+            db.collection("user").doc(user.uid).collection("requestForMe").doc(clicked_id).set({
+                reqDeclined: true,
+                reqCompleted: false
+            }, {
+                merge: true
+            })
+            db.collection("user").doc(user.uid).collection("requestForMe").doc(clicked_id).get().then(function (doc) {
+                var requesterId = doc.data().fromUserId;
+                var volunteerPostId = doc.data().volPostDocId;
+
+                console.log(requesterId);
+                console.log(volunteerPostId);
+                console.log(volName);
+                db.collection("user").doc(requesterId).collection("pastRequestsToOthers").where("volPostDocId", "==", volunteerPostId).get().
                 then(querySnapshot => {
                     querySnapshot.forEach(documentSnapshot => {
                         var querry = documentSnapshot.ref.path;
                         console.log(querry);
-                      console.log(`Found document at ${documentSnapshot.ref.path}`);
-                      var d = querry.split('/');
-                      console.log(d);
-                      var c = d[3];
-                      console.log(c);
-                      db.collection("user").doc(requesterId).collection("pastRequestsToOthers").doc(c).set(
-                        {reqAccepted:false,reqCompleted:false,volunteerName:volName},{merge:true}
-                      ).then(location.reload())
+                        console.log(`Found document at ${documentSnapshot.ref.path}`);
+                        var d = querry.split('/');
+                        console.log(d);
+                        var c = d[3];
+                        console.log(c);
+                        db.collection("user").doc(requesterId).collection("pastRequestsToOthers").doc(c).set({
+                            reqAccepted: false,
+                            reqCompleted: false,
+                            volunteerName: volName
+                        }, {
+                            merge: true
+                        }).then(location.reload())
+                    })
+
+
+                })
             })
-            
-            
+
         })
     })
-    
-})
-})
-    
+
 }
